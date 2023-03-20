a09bf207c913b4b167143e5338db748a92f68820
Dan Dang <dangvusidan@gmail.com>
2023-03-20 14:56:52 +0700

    first commit

---
+++ src/main/java/com/dhjo/dhjo_be/DhjoApplication.java
@@ -0,0 +1,13 @@
+package com.dhjo.dhjo_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhjo/dhjo_be/DhjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhjo.dhjo_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhjoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
